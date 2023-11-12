cf197cefd25710806a77d09d6eb248fc7ce635fd
o.likhogub <oligli@yandex.ru>
2023-11-12 16:56:20 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     cout << "Hello world! " << endl;
     heif_context* ctx = heif_context_alloc();
-    heif_context_read_from_file(ctx, "/mnt/c/Users/o.likhogub/Downloads/sample1.heic", nullptr);
+    heif_context_read_from_file(ctx, "/home/olikhogub/Downloads/sample1.heic", nullptr);
     heif_image_handle* handle;
     heif_context_get_primary_image_handle(ctx, &handle);
 
@@ -18,7 +18,7 @@ int main() {
     auto pngEncoder = new PngEncoder();
     pngEncoder->set_compression_level(5);
     encoder.reset(pngEncoder);
-    encoder->Encode(handle, img, "/mnt/c/Users/o.likhogub/Downloads/sample1.png");
+    encoder->Encode(handle, img, "/home/olikhogub/Downloads/sample1.png");
     heif_image_release(img);
     heif_image_handle_release(handle);
     return 0;
