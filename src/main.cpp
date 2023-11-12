729c2d46b348cfffb75b9979306b409de3954cf1
o.likhogub <oligli@yandex.ru>
2023-11-12 13:24:36 +0300

    Init

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <libheif/heif.h>
+#include "encoder_png.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello world! " << endl;
+    heif_context* ctx = heif_context_alloc();
+    heif_context_read_from_file(ctx, "/mnt/c/Users/o.likhogub/Downloads/sample1.heic", nullptr);
+    heif_image_handle* handle;
+    heif_context_get_primary_image_handle(ctx, &handle);
+
+    heif_image* img;
+    heif_decode_image(handle, &img, heif_colorspace_RGB, heif_chroma_interleaved_RGB, nullptr);
+    std::unique_ptr<Encoder> encoder;
+
+    auto pngEncoder = new PngEncoder();
+    pngEncoder->set_compression_level(5);
+    encoder.reset(pngEncoder);
+    encoder->Encode(handle, img, "/mnt/c/Users/o.likhogub/Downloads/sample1.png");
+    heif_image_release(img);
+    heif_image_handle_release(handle);
+    return 0;
+}
